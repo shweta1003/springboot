3f204a253af8895b00976eb44b7a66357d4bc0fe
SHWETA UMRAO <47379259+shweta1003@users.noreply.github.com>
2019-03-28 11:58:48 +0530

    Update BookRepository.java

--- SpringBoot/Author_Book-Project/Repository/BookRepository.java
+++ SpringBoot/Author_Book-Project/Repository/BookRepository.java
@@ -1,1 +1,25 @@
 
+package com.cur.opertaion.interfaces;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cur.opertaion.beans.Book;
+
+/**
+ * @author vishal
+ *
+ */
+@Repository
+@Transactional
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+	List<Book> findByPrice(Long price);
+
+	Book findByBookName(String bookName);
+
+}
