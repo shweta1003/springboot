5aa55235982e4468584b16b8f81b0a8c1977db13
SHWETA UMRAO <47379259+shweta1003@users.noreply.github.com>
2019-03-28 12:00:09 +0530

    Update Book.java

--- SpringBoot/Author_Book-Project/Beans/Book.java
+++ SpringBoot/Author_Book-Project/Beans/Book.java
@@ -1,1 +1,35 @@
 
+package com.cur.opertaion.beans;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+/**
+ * @author vishal
+ *
+ */
+@Entity
+@Data
+@Table(name = "book")
+public class Book {
+
+	/**
+	 * 
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "BOOK_SEQ")
+	@SequenceGenerator(name = "BOOK_SEQ", sequenceName = "BOOK_SEQ")
+	private Long bookId;
+
+	private String bookName;
+
+	private Long price;
+	
+
+}
