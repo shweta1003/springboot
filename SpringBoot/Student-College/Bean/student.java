2b63783edf0e6287dde98ca7bce2624b4c654f8b
SHWETA UMRAO <47379259+shweta1003@users.noreply.github.com>
2019-03-28 11:49:45 +0530

    Update and rename book.java to student.java

---
+++ SpringBoot/Student-College/Bean/student.java
@@ -0,0 +1,42 @@
+
+package com.insert.table.db.student;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.insert.table.db.collegeBean.College;
+
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name="student1")
+public class Student {
+	@Id
+	@GeneratedValue
+	private Long sid;
+	private String sname;
+	private String sbranch;
+	private int smarks;
+	//private int register_id;
+	
+	 @ManyToOne(fetch = FetchType.LAZY, optional = false)
+	    @JoinColumn(name = "cid", nullable = false)
+	    @OnDelete(action = OnDeleteAction.CASCADE)
+	    @JsonIgnore
+	private College college;
+	
+	public Student() {
+
+	}
+}
