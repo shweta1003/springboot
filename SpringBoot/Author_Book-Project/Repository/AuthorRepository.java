7a0f7808436684a8f2e6936ea146b8d7058cdb7c
SHWETA UMRAO <47379259+shweta1003@users.noreply.github.com>
2019-03-28 11:58:00 +0530

    Update AuthorRepository.java

--- SpringBoot/Author_Book-Project/Repository/AuthorRepository.java
+++ SpringBoot/Author_Book-Project/Repository/AuthorRepository.java
@@ -1,1 +1,29 @@
 
+package com.cur.opertaion.interfaces;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cur.opertaion.beans.Author;
+
+/**
+ * @author vishal
+ *
+ */
+@Repository
+@Transactional
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+	Author findByAuthorName(String authorName);
+
+	Iterable<Author> findByAuthorNameContaining(String authorName);
+
+	Iterable<Author> findByCity(String city);
+
+	Optional<Author> findByAuthorId(Long authorId);
+
+}
